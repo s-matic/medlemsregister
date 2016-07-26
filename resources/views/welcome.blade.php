0efc2b340edf586945537fdcaf98c07b59958e81
Stefan Matic <stefan.matic@me.com>
2016-07-26 20:14:05 +0200

    Translate auth scaffold to Swedish

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Välkommen</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    Lorem ipsum
                 </div>
             </div>
         </div>
