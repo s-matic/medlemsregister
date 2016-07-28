45cac831da162b603704b59f6c03b533bd5f749a
Stefan Matic <stefan.matic@me.com>
2016-07-28 21:20:20 +0200

    Add updated create members table migration

---
+++ database/migrations/2016_07_27_180449_create_members_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('date_of_birth');
+            $table->string('address');
+            $table->string('postal_code');
+            $table->string('city');
+            $table->string('telephone');
+            $table->string('email')->unique();
+            $table->integer('number')->unsigned();
+            $table->integer('organization_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('members');
+    }
+}
