9cf419914e52766ff06f30891e081009f65fd02b
Stefan Matic <stefan.matic@me.com>
2016-07-28 21:21:04 +0200

    Add create organizations table migration

---
+++ database/migrations/2016_07_27_175830_create_organizations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrganizationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organizations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('address');
+            $table->string('postal_code');
+            $table->string('city');
+            $table->string('email')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('organizations');
+    }
+}
