b67666db62aedd8e2d10ea6b086fd6435dc3e53a
Stefan Matic <stefan.matic@me.com>
2016-08-01 14:07:56 +0200

    Add laracasts/flash package

--- config/app.php
+++ config/app.php
@@ -156,6 +156,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Packages
+         */
+        Laracasts\Flash\FlashServiceProvider::class,
+
     ],
 
     /*
