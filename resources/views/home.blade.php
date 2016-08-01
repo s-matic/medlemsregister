d2606fe0ee11afc1ebd5b53d6eeb8cbd927a4055
Stefan Matic <stefan.matic@me.com>
2016-08-01 14:11:19 +0200

    Include flash message in home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,9 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
+            @include('flash::message')
             <div class="panel panel-default">
                 <div class="panel-heading">Översikt</div>
-
                 <div class="panel-body">
                     Du är inloggad!
                 </div>
