98b9c840ea54b5f8a9d2757d4f8bb6c9afa4434f
Stefan Matic <stefan.matic@me.com>
2016-08-28 15:48:28 +0200

    Add hashID to Member model

--- app/Member.php
+++ app/Member.php
@@ -3,9 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Vinkla\Hashids\Facades\Hashids;
 
 class Member extends Model
 {
+    /**
+     * Get the value of the model's route key.
+     *
+     * @return mixed
+     */
+    public function getRouteKey()
+    {
+        return Hashids::encode($this->getKey());
+    }
+
     /**
      * The attributes that are mass assignable.
      *
