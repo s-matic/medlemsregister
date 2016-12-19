b7ec8c0b77cc369648a9feac648a11e5425041fa
Stefan Matic <stefan.matic@me.com>
2016-12-19 18:36:50 +0100

    Update routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,4 +1,6 @@
 <?php
+use Illuminate\Support\Facades\URL;
+use Vinkla\Hashids\Facades\Hashids;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,16 +13,35 @@
 |
 */
 
+/*
+ * Route model bindnings to support Hashids
+ * TODO: Extend router to include custom function for wildcard models
+ */
+Route::bind('member', function($value, $route)
+{
+    $id = Hashids::decode($value)[0];
+    return \App\Member::findOrFail($id);
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::auth();
 
-Route::get('/home', 'HomeController@index');
-Route::get('/setup/organization', 'OrganizationController@create');
-Route::post('/setup/organization', 'OrganizationController@store');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/setup/organization', 'OrganizationController@create');
+    Route::post('/setup/organization', 'OrganizationController@store');
 
-Route::group(['middleware' => ['auth', 'requireOrg']], function () {
-    Route::resource('member', 'MemberController');
+    Route::group(['middleware' => 'requireOrg'], function () {
+        Route::get('member/create/printable', 'MemberController@createPrintable');
+        Route::resource('member', 'MemberController');
+        Route::resource('form', 'FormController');
+
+        Route::get('/home', 'HomeController@index');
+    });
+});
+
+Route::get('test', function(){
+    return Url::route('member.show', [\App\Member::find(4)]);
 });
