e4b01ab4ff86ef743e959b3ff320096b1b67e14e
Stefan Matic <stefan.matic@me.com>
2016-12-19 18:17:09 +0100

    Update member model

--- app/Member.php
+++ app/Member.php
@@ -23,6 +23,50 @@ class Member extends Model
      * @var array
      */
     protected $fillable = [
-        'first_name', 'last_name', 'date_of_birth', 'address', 'postal_code', 'city', 'telephone', 'email', 'number'
+        'first_name', 'last_name', 'date_of_birth', 'address', 'postal_code', 'city', 'telephone', 'email', 'number', 'membership', 'interests'
     ];
+
+    /*
+     * Accessors
+     */
+    public function getNameAttribute()
+    {
+        return $this->first_name.' '.$this->last_name;
+    }
+
+    public function paidFee()
+    {
+        $fee = $this->fees()->where('year', date('Y'))->first();
+
+        if(!isset($fee) || !$fee)
+            return false;
+
+        if($fee->paid)
+            return true;
+
+        return false;
+    }
+
+    public function hasInterest($name)
+    {
+        $key = array_search($name, unserialize($this->interests));
+        if($key)
+            return true;
+
+        return false;
+    }
+
+    /*
+     * Eloquent relationships
+     */
+    public function organization()
+    {
+        return $this->belongsTo('App\Organization');
+    }
+
+    public function fees()
+    {
+        return $this->hasMany('App\Fee');
+    }
+
 }
